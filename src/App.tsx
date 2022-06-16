879799a7a4cac4d75221574e8b2edd7f87c0ecf3
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2022-06-16 22:19:46 +0200

    perf: :zap: `Calculator.tsx` is now dynamically imported to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Calculator from "./Calculator";
+import { lazy, Suspense } from "react";
+import Spinner from "./components/Spinner";
+
+const Calculator = lazy(() => import("./Calculator"));
 
 const App: React.FC = () => {
   return (
     <div className="w-full p-6 bg-slate-100 min-h-screen flex flex-col items-center gap-6 justify-center outline-none">
       <p className="text-4xl font-semibold">simple calculator</p>
-      <Calculator />
+      <Suspense fallback={<Spinner />}>
+        <Calculator />
+      </Suspense>
     </div>
   );
 };
